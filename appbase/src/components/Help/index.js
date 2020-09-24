5d3d05c24023bdc4e8037d6021668f1668e18769
Rodrigo Franco <rodrigofrancodelima@gmail.com>
2020-09-24 18:21:06 -0300

    Iniciando Requesito numero 4

--- appbase/src/components/Help/index.js
+++ appbase/src/components/Help/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView, TextInput} from 'react-native';
+import style from './style';
+import Header from '../Header';
 
 const Help = () => {
     return (
-        <View>
-            <Text>Help</Text>
-        </View>
+        <>
+            <Header />
+            <ScrollView style={style.container}>
+                <Text>Pedido</Text>
+                <Text>Pagamento</Text>
+                <Text>Entrega</Text>
+                <Text>Contato</Text>
+            </ScrollView>
+        </>
     )
 }
 
